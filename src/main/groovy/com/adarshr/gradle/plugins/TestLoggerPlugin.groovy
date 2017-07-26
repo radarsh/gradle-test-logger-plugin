c6906a7e3c8139f42a0bc7fc671cd39f9886a7a9
radarsh <adarsh.ramamurthy@gmail.com>
2017-07-26 22:59:24 +0100

    Create plugin template

---
+++ src/main/groovy/com/adarshr/gradle/plugins/TestLoggerPlugin.groovy
@@ -0,0 +1,12 @@
+package com.adarshr.gradle.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class TestLoggerPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
