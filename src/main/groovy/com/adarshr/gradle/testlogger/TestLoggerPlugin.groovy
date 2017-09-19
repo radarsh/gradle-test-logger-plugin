bf543a81a474ad28be0c9384b336b00eea6e729f
radarsh <adarsh.ramamurthy@gmail.com>
2017-09-19 22:37:31 +0100

    Rename classes

--- src/main/groovy/com/adarshr/gradle/testlogger/TestLoggerPlugin.groovy
+++ src/main/groovy/com/adarshr/gradle/testlogger/TestLoggerPlugin.groovy
@@ -1,5 +1,6 @@
 package com.adarshr.gradle.testlogger
 
+import com.adarshr.gradle.testlogger.logger.TestEventLogger
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
@@ -16,7 +17,7 @@ class TestLoggerPlugin implements Plugin<Project> {
             assertSequentialTestExecution(test)
 
             test.testLogging.lifecycle.events = []
-            test.addTestListener(new TestLoggerTestListener(project))
+            test.addTestListener(new TestEventLogger(project))
         }
     }
 
