16f0b77c99f9144624bf30628f5fba7156c35f44
radarsh <adarsh.ramamurthy@gmail.com>
2017-09-22 15:17:40 +0100

    Clean up tests

--- src/functional-test/groovy/com/adarshr/gradle/testlogger/TestLoggerPluginSpec.groovy
+++ src/functional-test/groovy/com/adarshr/gradle/testlogger/TestLoggerPluginSpec.groovy
@@ -1,75 +1,27 @@
 package com.adarshr.gradle.testlogger
 
-import org.gradle.testkit.runner.BuildResult
-import org.gradle.testkit.runner.GradleRunner
-import spock.lang.Specification
-
-import static groovy.json.StringEscapeUtils.escapeJava
 import static org.gradle.testkit.runner.TaskOutcome.FAILED
 
-class TestLoggerPluginSpec extends Specification {
-
-    /*
-    - Test theme switching by configuration
-    - Test theme switching based on console flag
-     */
-    private static final char E = '\u001B'
+class TestLoggerPluginSpec extends AbstractFunctionalSpec {
 
     def "log spock tests"() {
         when:
             def result = run('sample-spock-tests')
         then:
-            def actual = getLoggerOutput(result.output, ':test', '6 tests completed, 2 failed, 2 skipped')
-            def expected = """
-                $E[1;93mcom.adarshr.test.FirstSpec$E[m
-                $E[1m  Test $E[mthis test should pass$E[33m STARTED$E[m$E[F
-                $E[2K$E[1m  Test $E[mthis test should pass$E[32m PASSED$E[m
-                $E[1m  Test $E[mthis test should fail$E[33m STARTED$E[m$E[F
-                $E[2K$E[1m  Test $E[mthis test should fail$E[31m FAILED$E[m
-                $E[1m  Test $E[mthis test should be skipped$E[33m STARTED$E[m$E[F
-                $E[2K$E[1m  Test $E[mthis test should be skipped$E[33m SKIPPED$E[m
-                $E[1;93mcom.adarshr.test.SecondSpec$E[m
-                $E[1m  Test $E[mthis test should pass$E[33m STARTED$E[m$E[F
-                $E[2K$E[1m  Test $E[mthis test should pass$E[32m PASSED$E[m
-                $E[1m  Test $E[mthis test should fail$E[33m STARTED$E[m$E[F
-                $E[2K$E[1m  Test $E[mthis test should fail$E[31m FAILED$E[m
-                $E[1m  Test $E[mthis test should be skipped$E[33m STARTED$E[m$E[F
-                $E[2K$E[1m  Test $E[mthis test should be skipped$E[33m SKIPPED$E[m
-            """.stripIndent()
-            normalize(actual) == normalize(expected)
+            def actual = getLoggerOutput(result.output)
+        and:
+            actual[0] == render('[bold,bright-yellow]com.adarshr.test.FirstSpec[/]')
+            actual[1] == render('')
+            actual[2] == render('[bold]  Test [/]this test should pass[erase-ahead,green] PASSED[/]')
+            actual[3] == render('[bold]  Test [/]this test should fail[erase-ahead,red] FAILED[/]')
+            actual[4] == render('[bold]  Test [/]this test should be skipped[erase-ahead,yellow] SKIPPED[/]')
+            actual[5] == render('')
+            actual[6] == render('[bold,bright-yellow]com.adarshr.test.SecondSpec[/]')
+            actual[7] == render('')
+            actual[8] == render('[bold]  Test [/]this test should pass[erase-ahead,green] PASSED[/]')
+            actual[9] == render('[bold]  Test [/]this test should fail[erase-ahead,red] FAILED[/]')
+            actual[10] == render('[bold]  Test [/]this test should be skipped[erase-ahead,yellow] SKIPPED[/]')
         and:
             result.task(":test").outcome == FAILED
-    }
-
-    private static BuildResult run(String project) {
-        GradleRunner.create()
-            .withGradleVersion('4.0.2')
-            .withProjectDir(new File("src/test/resources/${project}"))
-            .withPluginClasspath()
-            .withDebug(true)
-            .withArguments('clean', 'test')
-            .forwardOutput()
-            .buildAndFail()
-    }
-
-    private static String getLoggerOutput(String text, String startMarker, String endMarker) {
-        def lines = text.readLines()
-        def loggerLines = lines.subList(lines.indexOf(startMarker) + 1, lines.indexOf(endMarker) - 1)
-        loggerLines.join('\n')
-    }
-
-    private static String normalize(String input) {
-        def escaped = escapeJava(input).replace('\\n', '\n').trim()
-        def groupedBySpec = [:]
-
-        escaped.readLines().each { line ->
-            if (line.contains('Spec')) {
-                groupedBySpec."${line}" = [line]
-            } else {
-                groupedBySpec[groupedBySpec.keySet().last()] << line
-            }
-        }
-
-        groupedBySpec.sort { it.key }.values().flatten().join('\n')
     }
 }
--- src/functional-test/groovy/com/adarshr/gradle/testlogger/ThemeSwitchingSpec.groovy
+++ src/functional-test/groovy/com/adarshr/gradle/testlogger/ThemeSwitchingSpec.groovy
@@ -1,91 +1,44 @@
 package com.adarshr.gradle.testlogger
 
-import com.adarshr.gradle.testlogger.renderer.AnsiTextRenderer
-import org.apache.commons.io.FileUtils
-import org.gradle.testkit.runner.BuildResult
-import org.gradle.testkit.runner.GradleRunner
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
-import spock.lang.Specification
-
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
-class ThemeSwitchingSpec extends Specification {
-
-    private static final def START_MARKER = '[[ START ]]'
-    private static final def END_MARKER = '[[ END ]]'
-
-    private static final def MARKERS = """
-        test {
-            doFirst {
-                logger.lifecycle '$START_MARKER'
-            }
-            doLast {
-                logger.lifecycle '$END_MARKER'
-            }
-        }
-    """
-
-    @Rule
-    TemporaryFolder temporaryFolder
+class ThemeSwitchingSpec extends AbstractFunctionalSpec {
 
     def "log spock tests when plain theme is set"() {
         when:
-            def result = run('single-spock-test', """
-                testlogger {
-                    theme 'plain'
-                }
-                
-                $MARKERS
-            """)
+            def result = run('single-spock-test', "testlogger { theme 'plain' }")
         then:
             def actualLines = getLoggerOutput(result.output)
             actualLines.size() == 2
-            actualLines[0] == 'com.adarshr.test.SingleSpec'
-            actualLines[1] == '  Test this is a single test PASSED'
+            actualLines[0] == render('com.adarshr.test.SingleSpec')
+            actualLines[1] == render('  Test this is a single test PASSED')
         and:
             result.task(":test").outcome == SUCCESS
     }
-
-    def ansi = new AnsiTextRenderer()
 
     def "log spock tests when standard theme is set"() {
         when:
-            def result = run('single-spock-test', """
-                testlogger {
-                    theme 'standard'
-                }
-                
-                $MARKERS
-            """)
+            def result = run('single-spock-test', "testlogger { theme 'standard' }")
         then:
             def actualLines = getLoggerOutput(result.output)
             actualLines.size() == 3
-            actualLines[0] == ansi.render('[bold,bright-yellow]com.adarshr.test.SingleSpec[/]')
-            actualLines[1] == ansi.render('[bold]  Test [/]this is a single test[yellow] STARTED[/][cursor-up-line]')
-            actualLines[2] == ansi.render('[erase-line,bold]  Test [/]this is a single test[green] PASSED[/]')
+            actualLines[0] == render('[bold,bright-yellow]com.adarshr.test.SingleSpec[/]')
+            actualLines[1] == render('')
+            actualLines[2] == render('[bold]  Test [/]this is a single test[erase-ahead,green] PASSED[/]')
         and:
             result.task(":test").outcome == SUCCESS
     }
 
-    private BuildResult run(String project, String buildFragment) {
-        def projectDir = new File(temporaryFolder.root, project)
-        FileUtils.copyDirectoryToDirectory(new File("src/test/resources/${project}"), temporaryFolder.root)
-        def buildFile = new File(projectDir, 'build.gradle')
-        buildFile << buildFragment
-
-        GradleRunner.create()
-            .withGradleVersion('4.0.2')
-            .withProjectDir(projectDir)
-            .withPluginClasspath()
-            .withDebug(true)
-            .withArguments('clean', 'test')
-            .forwardOutput()
-            .build()
-    }
-
-    private static List<String> getLoggerOutput(String text) {
-        def lines = text.readLines()
-        lines.subList(lines.indexOf(START_MARKER) + 1, lines.indexOf(END_MARKER))
+    def "log spock tests when mocha theme is set"() {
+        when:
+            def result = run('single-spock-test', "testlogger { theme 'mocha' }")
+        then:
+            def actualLines = getLoggerOutput(result.output)
+            actualLines.size() == 3
+            actualLines[0] == render('  [bold]com.adarshr.test.SingleSpec[/]')
+            actualLines[1] == render('')
+            actualLines[2] == render('    [erase-ahead,green]✔[/] this is a single test')
+        and:
+            result.task(":test").outcome == SUCCESS
     }
 }
