201c54ce581a66908af94d2d8a3d2eb190afe7d9
Goooler <wangzongler@gmail.com>
2023-10-12 16:25:26 +0800

    Add min Gradle version checker
    To align with 89d2671c10ba525276ff27e398a239cea9312776.

--- src/main/groovy/com/adarshr/gradle/testlogger/TestLoggerPlugin.groovy
+++ src/main/groovy/com/adarshr/gradle/testlogger/TestLoggerPlugin.groovy
@@ -2,17 +2,24 @@ package com.adarshr.gradle.testlogger
 
 import com.adarshr.gradle.testlogger.logger.TestLoggerWrapper
 import groovy.transform.CompileStatic
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
+import org.gradle.util.GradleVersion
 
 @CompileStatic
 class TestLoggerPlugin implements Plugin<Project> {
 
     private static final String EXTENSION_NAME = 'testlogger'
+    private static final String MIN_GRADLE = "7.6"
 
     @Override
     void apply(Project project) {
+        if (GradleVersion.current() <= GradleVersion.version(MIN_GRADLE)) {
+            throw new GradleException("Gradle Test Logger Plugin requires Gradle version ${MIN_GRADLE} or above.")
+        }
+
         project.extensions.create(EXTENSION_NAME, TestLoggerExtension, project)
 
         project.tasks.withType(Test).configureEach { Test test ->
